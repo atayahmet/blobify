840ae959619b8cd346f8dde655fc1a3222359d92
Ahmet ATAY <ahmet@efabrika.com>
2019-03-23 22:44:02 +0300

    Added documentation. Test assertions has been improvement.

--- src/blobify.ts
+++ src/blobify.ts
@@ -1,58 +1,87 @@
-interface ICreateBlobParams {
-  chars: string;
-  slice: number;
-  type: string;
+export interface IChunkOptions {
+  chars: string | Uint8Array;
+  slice?: number;
 }
+
+export type CatchFunctionType = (e: () => any) => any;
+export type PipeFunctionType = (data: any) => any;
 
 export default class Blobify {
   public static BLOB_PART_SIZE = 512;
-  public static catcher = (e: any) => e;
+  private data: any;
+  private pipes: any[] = [];
 
-  public static pipe(func: any) {
-    Blobify.pipes.push(func);
+  /**
+   * Pipeline itarator method
+   * @param func Function
+   * @return this
+   */
+  public pipe(func: PipeFunctionType): Blobify {
+    this.pipes.push(func);
     return this;
   }
 
-  public static catch(func: () => any) {
-    Blobify.catcher = func;
+  /**
+   * A handler function when throws any exception
+   * @param func Function
+   * @return this
+   */
+  public catch(func: CatchFunctionType): Blobify {
+    this.catcher = func;
     return this;
   }
 
-  public static async run(cb?: any): Promise<any> {
+  /**
+   * Run all tasks in pipeline and return promise.
+   * If you want to use a callback instead of promise, just pass a callback function.
+   * @param cb Function
+   * @return any|void
+   */
+  public async run(cb?: any): Promise<any | void> {
     try {
-      for (const pipe of this.pipes) {
-        const result = pipe(this.data);
+      for (const pipeFunc of this.pipes) {
+        const result = pipeFunc(this.data);
         this.data = result instanceof Promise ? await result : result;
       }
       this.pipes = [];
-      this.catcher = (e: any) => e;
+      this.catcher = (e: CatchFunctionType) => e;
       return cb ? cb(this.data) : this.data;
     } catch (e) {
       this.catcher(e);
     }
   }
 
-  public static removeBase64Data(str: string): string {
-    return str.split(',')[1];
-  }
-
-  private static data: any;
-  private static pipes: any[] = [];
+  private catcher: CatchFunctionType = (e: object) => e;
 }
 
+export function pipe(func: PipeFunctionType): Blobify {
+  return new Blobify().pipe(func);
+}
+
+/**
+ * Create blob object with passed chunked data
+ * @param chunks array
+ * @param type string
+ * @return Blob
+ */
 export function createBlob(chunks: string[], type: string): Blob {
   return new Blob(chunks, { type });
 }
 
-export function chunk(params: any) {
-  const { chars, slice }: ICreateBlobParams = params;
+/**
+ * Create chunks using passed stream data.
+ * If passed data is a Uint8Array instance, just it wrap with plain array.
+ * @param params
+ */
+export function chunk(params: IChunkOptions): Uint8Array[] {
+  const { chars, slice } = params;
 
-  if ((chars as any) instanceof Uint8Array) {
+  if (chars instanceof Uint8Array) {
     return [chars];
   }
 
   const byteArrays: Uint8Array[] = [];
-  const partSize: number = slice || Blobify.BLOB_PART_SIZE;
+  const partSize: number = slice && typeof slice === 'number' ? slice : Blobify.BLOB_PART_SIZE;
 
   for (let offset = 0; offset < chars.length; offset += partSize) {
     // skip until part size and slice by part size.
@@ -74,6 +103,10 @@ export function chunk(params: any) {
   return byteArrays;
 }
 
+/**
+ * Convert native file object to base64 with promise.
+ * @param file
+ */
 export function base64Encode(file: File): Promise<string> {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
@@ -83,11 +116,30 @@ export function base64Encode(file: File): Promise<string> {
   });
 }
 
-export function base64Decode(base64: string): any | ExceptionInformation {
-  return atob(Blobify.removeBase64Data(base64));
+/**
+ * Convert base64 to binary string
+ * @param base64 string
+ * @return string | ExceptionInformation
+ */
+export function base64Decode(base64: string): string | ExceptionInformation {
+  return atob(removeBase64Data(base64));
 }
 
-export function toStream(blob: Blob) {
+/**
+ * Remove base64 data prefix. (data:image/jpeg;base64,)
+ * More info: https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs
+ * @param str string
+ * @return string
+ */
+export function removeBase64Data(str: string): string {
+  return str.replace(/(^data\:.*\;base64,)/, '');
+}
+
+/**
+ * Convert blob object to interpretable string.
+ * @param blob
+ */
+export function toStream(blob: Blob): Promise<object> {
   const reader = new FileReader();
   const readerPromise = new Promise((resolve, reject) => {
     reader.onload = e => resolve(reader.result || '');
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-export const Greeter = (name: string) => `Hello ${name}`;
+import * as blobify from './blobify';
+
+const { base64Decode, base64Encode, createBlob, chunk, pipe, removeBase64Data, toStream } = blobify;
+
+export { base64Decode, base64Encode, createBlob, chunk, pipe, removeBase64Data, toStream };
